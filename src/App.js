86067cb12f7488e0906c3c71afc1da5757d960e5
Asta <74164885+asta-li@users.noreply.github.com>
2023-07-06 07:48:21 -0400

    Isolate the estimateGas issue

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {JsonRpcProvider} from '@ethersproject/providers';
+
 function App() {
+
+  const test = async () => {
+
+    // This unexpectedly fails if you uncomment the "from" field
+    const txRequest = {
+      data: "0x40c10f19000000000000000000000000e1924b1d35f6dd6786f0c81a4b14c009b76ba3000000000000000000000000000000000000000000000000000000000000000001",
+      // from: "0xe1924b1d35f6dd6786f0c81a4b14c009b76ba300",
+      to: "0xa4c3bf7aaa7ff67dded9d805e6dd4c79832917f4",
+      value: "0x11c37937e08000",
+    };
+    console.log("Transaction", txRequest);
+
+    const provider = new JsonRpcProvider('https://goerli.infura.io/v3/4df5e2316331463a9130964bd6078dfa');
+    const gasEstimate = await provider.estimateGas(txRequest);
+    console.log("Gas estimate", gasEstimate);
+  }; 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={test}
         >
-          Learn React
-        </a>
+          Test estimateGas
+        </button>
       </header>
     </div>
   );
